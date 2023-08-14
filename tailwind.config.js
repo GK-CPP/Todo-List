983c01a10b68cc650c0386bbfaa40f13ffec29b0
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-08-15 03:56:45 +0600

    2nd commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      height: {
+        22 : "80px",
+      },
+      screens: {
+        'xs': '13px', 
+      },
+    },
+  },
+  plugins: [],
+}
+
