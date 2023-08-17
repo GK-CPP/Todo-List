a9431554a6a8d1a0cf69b3b35be1d69afcb9eb63
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-08-17 18:40:02 +0600

    delete page js done

--- index.js
+++ index.js
@@ -5,28 +5,72 @@ var taskArray = [];
 //Element Push Function
 function elementPush(functionClass, textInput) {
   let newDiv = document.createElement("div");
+  let newDivLeft = document.createElement("div");
+  let newDivRight = document.createElement("div");
+
   let newH1 = document.createElement("h1");
   let imgElement = document.createElement("img");
+  let imgElementDelete = document.createElement("img");
+  let imgElementEdit = document.createElement("img");
 
   imgElement.src = "image/checklist.png";
   imgElement.alt = "Image description";
   imgElement.setAttribute("data-id", textInput.id);
   imgElement.classList.add("checklistImgClass");
   imgElement.classList.add("cursor-pointer");
-  //console.log("check2");
-  //console.log(imgElement);
 
-  newDiv.classList.add("flex");
-  newDiv.classList.add("rounded-3xl");
   imgElement.classList.add("w-8");
   imgElement.classList.add("h-8");
   imgElement.classList.add("mt-[1px]");
   imgElement.classList.add("mr-2");
+  //console.log("check2");
+  //console.log(imgElement);
+  imgElementDelete.src = "image/delete.png";
+  imgElementDelete.alt = "Image description delete";
+  imgElementDelete.setAttribute("data-id-delete", textInput.id);
+  imgElementDelete.classList.add("checklistImgDelete");
+  imgElementDelete.classList.add("cursor-pointer");
+  imgElementDelete.setAttribute("onclick", `deleteTask(${textInput.id})`);
+
+  imgElementDelete.classList.add("w-8");
+  imgElementDelete.classList.add("h-8");
+  imgElementDelete.classList.add("mt-[1px]");
+  imgElementDelete.classList.add("mr-2");
+
+  console.log(imgElement);
+
+  imgElementEdit.src = "image/edit.png";
+  imgElementEdit.alt = "Image description edit";
+  imgElementEdit.setAttribute("data-id-edit", textInput.id);
+  imgElementEdit.classList.add("checklistImgEdit");
+  imgElementEdit.classList.add("cursor-pointer");
+
+  imgElementEdit.classList.add("w-8");
+  imgElementEdit.classList.add("h-8");
+  imgElementEdit.classList.add("mt-[1px]");
+  imgElementEdit.classList.add("mr-2");
+
+  newDiv.classList.add("flex");
+  newDiv.classList.add("rounded-3xl");
+  newDiv.classList.add("justify-between");
+
+  // apply id to the div
+  newDiv.id = textInput.id;
+
+  newDivLeft.classList.add("flex");
+
+  newDivRight.classList.add("flex");
+
   newH1.className = " border-gray-400 rounded-sm text-xl text-white";
 
   newH1.innerText = textInput._value;
-  newDiv.appendChild(imgElement);
-  newDiv.appendChild(newH1);
+  newDivLeft.appendChild(imgElement);
+  newDivLeft.appendChild(newH1);
+  newDivRight.appendChild(imgElementEdit);
+  newDivRight.appendChild(imgElementDelete);
+
+  newDiv.appendChild(newDivLeft);
+  newDiv.appendChild(newDivRight);
   document.getElementById(functionClass).appendChild(newDiv);
 }
 
@@ -59,13 +103,27 @@ textButton.addEventListener("click", function () {
   }
 });
 
+//for delete task
+const deleteTask = (id) => {
+  const _div = document.getElementById(id);
+  console.log(_div);
+  _div.remove();
+  let tasksString = localStorage.getItem("taskId");
+  taskArray = JSON.parse(tasksString);
+  taskArray = taskArray.filter((taskArrayId) => taskArrayId.id != id);
+  localStorage.setItem("taskId", JSON.stringify(taskArray));
+};
+
 //Local Storage load function
 window.onload = function () {
   let tasksString = localStorage.getItem("taskId");
   if (tasksString) {
     taskArray = JSON.parse(tasksString);
     for (let i = 0; i < taskArray.length; i++) {
-      elementPush("task", taskArray[i]);
+      if (taskArray[i].completed == false) {
+        //console.log("task-33", taskArray[i].completed);
+        elementPush("task", taskArray[i]);
+      }
     }
     //var checklistDataId = 0;
     //Find all elements with class "checklistImgClass" and add click event listener
@@ -95,17 +153,3 @@ window.onload = function () {
     });
   }
 };
-
-//     console.log();
-//     for (let i = 0; i < taskArray.length; i++) {
-//       if (taskArray[i].id == checklistDataId) {
-//         taskArray[i].completed = true;
-//       }
-//       if (taskArray[i].completed == false) {
-//         elementPush("task", taskArray[i]);
-//       } else {
-//         elementPush("task-completed", taskArray[i]);
-//       }
-//     }
-//   }
-// };
